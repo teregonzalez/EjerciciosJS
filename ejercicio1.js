7eeb274415202d653bf0b55d06b098412e2ef9b7
teregonzalez <teregonzalezpro@gmail.com>
2022-02-14 09:59:50 -0300

    Se agregan test

--- ejercicio1.js
+++
@@ -1,9 +0,0 @@
-const reverseString = (someString)  => {
-    reversed =""
-
-    for(let i=someString.length -1 ; i >= 0; i--){
-        reversed += someString[i];
-    }
-    return reversed
-}
-console.log(reverseString("Hola"));
